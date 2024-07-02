1944ceafe6e551ad1dd8d566ff2d273afefe1158
sensei_blaq <jnr.amamoo@gmail.com>
2024-07-02 13:27:57 +0100

    first commit - day one activiities

---
+++ hello.py
@@ -0,0 +1,25 @@
+# Day 01
+    # Basic Arithmetic
+print (2 + 3)
+print (3 - 2)
+print (3 * 4)
+print (2 ** 3)
+print (12 / 2)
+print (12 // 6)
+print (13 % 5)
+    # Data types
+print(type((2)))
+print(type((-3.5)))
+print(type((4 + 2J)))
+print(type(True))
+print(type(('Hello world')))
+print(type(['banana', 'apple', 'cocnut']))
+print(type({'first_name': 'Harry', 'last_name': 'Dixon', 'age': 19}))
+print(type(('God', 4, 'life')))
+print(type({'ask', 'back', 'come', 'down', 'east'}))
+
+#Elucidian Distaance = (x2 -x1)^2 + (y2 - y2)^2
+    #Calculate the Eucidian distance between (2,3) and (10,8)
+print((((10 - 2)**2 + (8 - 3)**2)**0.5))
+
+
