696246c258587a3b53e2e0339c4c334804b2f9d4
sensei_blaq <jnr.amamoo@gmail.com>
2024-07-10 10:58:03 +0100

    day 3

---
+++ operators.py
@@ -0,0 +1,36 @@
+# age = 68
+# height = 190.0
+# comp_numb = 4 + 2j
+
+# #area of triangle = 0.5(b * H)
+# base_triangle = int(input('Enter base of the triangle: '))
+# height_triangle = int(input('Enter height of the triangle: '))
+# area_triangle = 0.5 * (base_triangle * height_triangle)
+# print(area_triangle)
+
+# #perimeter of triangle = a + b + c
+# a = int(input('Enter side 1:'))
+# b = int(input('Enter side 2:'))
+# c = int(input('Enter side 3:'))
+# perimeter = a + b + c
+# print(perimeter)
+
+# #area_rect = length * width peri-rect = 2 * area _rect
+# length = int(input('Enter the length of the rectangle: '))
+# width = int(input('Enter the width of the rectangle: '))
+# area_rect = length * width
+# peri_rect = 2 * area_rect
+# print(f'The area of the rectangle is {area_rect} and the perimeter is {peri_rect}')
+
+# # (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14
+# radius = int(input('Enter the radius of a circle: '))
+# area_circ = 3.14 * radius ** 2
+# circum_circ = 2 * 3.14 * radius
+# print (f'The area of the circle is {area_circ} and cicrcumference of the circle is {circum_circ}')
+
+#linar eqn is y = mx + b, b is  y intercept, x intercept = -b/m where y = 0
+#e.g y = 2x - 2
+x_intercept = -(-2)/2
+print (f'the x intercept for the eqn y = 2x -2 is ({x_intercept}, 0) and the y intercept is (0,2)')
+
+print ('End of program!!!')
